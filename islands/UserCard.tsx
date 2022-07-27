9eabcdbefec268ccf11ee2502428616228281570
Micurran <mcurran1722@gmail.com>
2022-07-26 20:57:55 -0600

    advice generator app

--- islands/UserCard.tsx
+++ islands/UserCard.tsx
@@ -4,37 +4,84 @@ import { tw } from "@twind";
 import {useState, useEffect} from 'preact/hooks';
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
-interface IUserProps {
-    userImage: string
-}
 export default function UserCard() {
 const [user, setUser] = useState<any>(null);
 const [loaded, setLoaded] = useState<boolean>(false);
+const [hovered, setHovered] = useState<boolean>(false);
+
+const getAdvice = () => {
+  setLoaded(false);
+  axiod.get('https://api.adviceslip.com/advice').then((data) => {
+    setUser(data.data.slip.advice)
+    console.log(data.data.slip.advice)
+    setLoaded(true);
+});
+};
 
 useEffect(() => {
-  axiod.get('https://randomuser.me/api/').then((data) => {
-    setUser(data.data.results[0])
-    console.log(data.data.results[0])
+  setLoaded(false);
+  axiod.get('https://api.adviceslip.com/advice').then((data) => {
+    setUser(data.data.slip.advice)
+    console.log(data.data.slip.advice)
     setLoaded(true);
 });
 return () => {
-    'hello'
+    true
   }
 }, [])
   
   return (
     
-<div class={tw`max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700`}>
-    <div class={tw`flex flex-col items-center pb-10`}>
-        <img class={tw`mb-3 w-24 h-24 rounded-full shadow-lg`} src={loaded && user.picture.large} alt="Bonnie image"/>
-        <h5 class={tw`mb-1 text-xl font-medium text-gray-900 dark:text-white`}>{loaded && `${user?.name?.first} ${user?.name?.last}`}</h5>
-        <span class={tw`text-sm text-gray-500 dark:text-gray-400`}>Visual Designer</span>
+<div class={tw`w-2xl rounded-lg shadow-md `}
+style={{backgroundColor: 'hsl(217, 19%, 24%)'}}
+>
+    <div class={tw`flex flex-col items-center p-10 font-Manrope text-center`}
+    style={{color: 'hsl(193, 38%, 86%)', fontSize: '2.5rem',}}
+    >
+        <p class={tw`text-lg font-bold font-sans`} style={{color: 'hsl(150, 100%, 66%)'}}>Fresh Advice</p>
+        {loaded
+          ? <p>"{user}"</p>
+          : <p>loading...</p>
+        }
+                <div class={tw`flex mt-4 space-x-3 lg:mt-6`}>
+        <img
+        src="/patternDivider.svg"
+        height="100px"
+        alt="a line with a quotation mark in the center"
+      />
+      </div>
         <div class={tw`flex mt-4 space-x-3 lg:mt-6`}>
-            <a href="#" class={tw`inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Add friend</a>
-            <a href="#" class={tw`inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-70`}>Message</a>
+            <button
+              onClick={getAdvice}
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+              style={
+                hovered === true
+                ? {boxShadow: `0 5px 50px 2px hsl(150, 100%, 66%)`, backgroundColor: 'hsl(150, 100%, 66%)'}
+                : {backgroundColor: 'hsl(150, 100%, 66%)'}
+              }
+              href="#" class={tw`inline-flex items-center p-5 text-sm font-medium text-center text-white rounded-full`}>
+                <img
+        src="/iconDice.svg"
+        height="100px"
+        alt="Icon of a die"
+      />
+            </button>
         </div>
     </div>
 </div>
 
   );
 }
+
+
+/* ### Primary
+
+- Light Cyan: hsl(193, 38%, 86%)
+- Neon Green: hsl(150, 100%, 66%)
+
+### Neutral
+
+- Grayish Blue: hsl(217, 19%, 38%)
+- Dark Grayish Blue: hsl(217, 19%, 24%)
+- Dark Blue: hsl(218, 23%, 16%) */
--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,9 @@ import UserCard from '../islands/UserCard.tsx'
 export default function Home() {
 
   return (
-    <div class={tw`p-4 mx-auto flex flex-col justify-center items-center bg-green-500 min-h-screen`}>
+    <div class={tw`p-4 mx-auto flex flex-col justify-center items-center bg-green-500 min-h-screen`}
+    style={{backgroundColor: 'hsl(218, 23%, 16%)'}}
+    >
       <img
         src="/logo.svg"
         height="100px"
