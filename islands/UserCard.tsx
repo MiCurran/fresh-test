7f22e7eff21edc98a6cccde9873610cdc3f64ab1
Micurran <mcurran1722@gmail.com>
2022-07-26 22:13:04 -0600

    styling updates

--- islands/UserCard.tsx
+++ islands/UserCard.tsx
@@ -4,6 +4,8 @@ import { tw } from "@twind";
 import {useState, useEffect} from 'preact/hooks';
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
+const Quote = () => <span style={{fontSize: '28px', fontWeight: 700}}>"</span>
+
 export default function UserCard() {
 const [user, setUser] = useState<any>(null);
 const [loaded, setLoaded] = useState<boolean>(false);
@@ -19,12 +21,7 @@ const getAdvice = () => {
 };
 
 useEffect(() => {
-  setLoaded(false);
-  axiod.get('https://api.adviceslip.com/advice').then((data) => {
-    setUser(data.data.slip.advice)
-    console.log(data.data.slip.advice)
-    setLoaded(true);
-});
+getAdvice();
 return () => {
     true
   }
@@ -32,40 +29,52 @@ return () => {
   
   return (
     
-<div class={tw`w-2xl max-w-2xl rounded-lg shadow-md `}
-style={{backgroundColor: 'hsl(217, 19%, 24%)'}}
+<div 
+  class={tw`w-2xl max-w-2xl rounded-lg shadow-md `}
+  style={{backgroundColor: 'hsl(217, 19%, 24%)'}}
 >
-    <div class={tw`flex flex-col items-center p-10 font-Manrope text-center`}
-    style={{color: 'hsl(193, 38%, 86%)', fontSize: '2.5rem',}}
+    <div 
+      class={tw`flex flex-col items-center p-10 pb-0 font-Manrope text-center`}
+      style={{color: 'hsl(193, 38%, 86%)', fontSize: '2.5rem',}}
     >
-        <p class={tw`text-lg font-bold font-sans`} style={{color: 'hsl(150, 100%, 66%)'}}>Fresh Advice</p>
+      <div class={tw`p-4`}>
+        <p 
+          class={tw`text-lg font-bold font-sans`} 
+          style={{color: 'hsl(150, 100%, 66%)'}}
+        >
+            Fresh Advice
+        </p>
+        </div>
+        <div class={tw`p-4`}>
         {loaded
-          ? <p style={{fontWeight: 800}}>"{user}"</p>
+          ? <p style={{fontWeight: 800}}><Quote />{user}<Quote /></p>
           : <p>loading...</p>
         }
-                <div class={tw`flex mt-4 space-x-3 lg:mt-6`}>
-        <img
-        src="/patternDivider.svg"
-        height="100px"
-        alt="a line with a quotation mark in the center"
-      />
+        </div>
+        <div class={tw`flex mt-10 space-x-3 lg:mt-6`}>
+          <img
+            src="/patternDivider.svg"
+            height="100px"
+            alt="a line with a quotation mark in the center"
+          />
       </div>
-        <div class={tw`flex mt-4 space-x-3 lg:mt-6`}>
+        <div class={tw`flex space-x-3 mt-6 mb-[-30px]`}>
             <button
               onClick={getAdvice}
               onMouseEnter={() => setHovered(true)}
               onMouseLeave={() => setHovered(false)}
+              class={tw`inline-flex items-center p-5 text-sm font-medium text-center text-white rounded-full`}
               style={
                 hovered === true
                 ? {boxShadow: `0 5px 50px 2px hsl(150, 100%, 66%)`, backgroundColor: 'hsl(150, 100%, 66%)'}
                 : {backgroundColor: 'hsl(150, 100%, 66%)'}
               }
-              href="#" class={tw`inline-flex items-center p-5 text-sm font-medium text-center text-white rounded-full`}>
-                <img
-        src="/iconDice.svg"
-        height="100px"
-        alt="Icon of a die"
-      />
+            >
+              <img
+                src="/iconDice.svg"
+                height="100px"
+                alt="Icon of a die"
+              />
             </button>
         </div>
     </div>
--- routes/index.tsx
+++ routes/index.tsx
@@ -6,16 +6,17 @@ import UserCard from '../islands/UserCard.tsx'
 export const Head = () => (
   <head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com"/>
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
   </head>
 );
 
 export default function Home() {
 
   return (
-    <div class={tw`p-4 mx-auto flex flex-col justify-center items-center bg-green-500 min-h-screen`}
-    style={{backgroundColor: 'hsl(218, 23%, 16%)'}}
+    <div 
+      class={tw`p-4 mx-auto flex flex-col justify-center items-center bg-green-500 min-h-screen`}
+      style={{backgroundColor: 'hsl(218, 23%, 16%)'}}
     >
       <img
         src="/logo.svg"
